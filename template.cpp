b8bc74be76048b118195f19a792c10c794c72eb5
Anurag <anuraganandx@gmail.com>
2014-05-23 20:29:58 +0530

    Template Added

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair <int,int> pii;
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(a) (int)a.size()
+#define all(c) c.begin(), c.end()
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define rept(i, a, b) for(int i = (a); i < (b); i++)
+#define fill(a, v) memset(a, v, sizeof(a))
+#define foreach(c, it) for(__typeof(c.begin()) it = c.begin(); it != c.end(); ++it)
