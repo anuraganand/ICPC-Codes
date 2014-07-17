2624726fab2d3c21c342f256ebd4744062bcee70
Anurag <anuraganandx@gmail.com>
2014-07-17 22:34:07 +0530

    added Dinics Flow

---
+++ DinicsFlow.cpp
@@ -0,0 +1,72 @@
+const int inf = 0x3f3f3f3f;
+
+struct edge {
+    int x, y, cap, flow;
+};
+
+struct DinicFlow {
+    vector <edge> e;
+    vector <int> cur, d;
+    vector < vector <int> > adj;
+    int n, source, sink;
+
+    DinicFlow() {}
+
+    DinicFlow(int v) {
+        n = v;
+        cur = vector <int> (n + 1);
+        d = vector <int> (n + 1);
+        adj = vector < vector <int> > (n + 1);
+    }
+
+    void addEdge(int from, int to, int cap) {
+        edge e1 = {from, to, cap, 0};
+        edge e2 = {to, from, 0, 0};
+        adj[from].push_back(e.size()); e.push_back(e1);
+        adj[to].push_back(e.size()); e.push_back(e2);
+    }
+
+    int bfs() {
+        queue <int> q;
+        for(int i = 0; i <= n; ++i) d[i] = -1;
+        q.push(source); d[source] = 0;
+        while(!q.empty() and d[sink] < 0) {
+            int x = q.front(); q.pop();
+            for(int i = 0; i < (int)adj[x].size(); ++i) {
+                int id = adj[x][i], y = e[id].y;
+                if(d[y] < 0 and e[id].flow < e[id].cap) {
+                    q.push(y); d[y] = d[x] + 1;
+                }
+            }
+        }
+        return d[sink] >= 0;
+    }
+
+    int dfs(int x, int flow) {
+        if(!flow) return 0;
+        if(x == sink) return flow;
+        for(;cur[x] < (int)adj[x].size(); ++cur[x]) {
+            int id = adj[x][cur[x]], y = e[id].y;
+            if(d[y] != d[x] + 1) continue;
+            int pushed = dfs(y, min(flow, e[id].cap - e[id].flow));
+            if(pushed) {
+                e[id].flow += pushed;
+                e[id ^ 1].flow -= pushed;
+                return pushed;
+            }
+        }
+        return 0;
+    }
+
+    int maxFlow(int src, int snk) {
+        this->source = src; this->sink = snk;
+        int flow = 0;
+        while(bfs()) {
+            for(int i = 0; i <= n; ++i) cur[i] = 0;
+            while(int pushed = dfs(source, inf)) {
+                flow += pushed;
+            }
+        }
+        return flow;
+    }
+};
